d2814a8066bd28aea359df46c05bb386f120e712
Xuebin Wei <weixuebin@gmail.com>
2017-09-13 09:38:11 -0400

    Update Query_in_Python.py

--- Access/Query_in_Python.py
+++ Access/Query_in_Python.py
@@ -2,7 +2,7 @@ import pyodbc  # using the pyodbc library
 
 db_file = '' #define the location of your Access file
 
-odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb)};DBQ=%s' %(db_file) # define the odbc connection parameter
+odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s' %(db_file) # define the odbc connection parameter
 
 conn = pyodbc.connect(odbc_conn_str) # establish a database connection
 
