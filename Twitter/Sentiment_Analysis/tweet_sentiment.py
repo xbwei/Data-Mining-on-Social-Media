58fe662d4717b63f616c472dc946d78cd0991703
Xuebin Wei <weixuebin@gmail.com>
2018-11-05 11:29:19 -0500

    Create tweet_sentiment.py

---
+++ Twitter/Sentiment_Analysis/tweet_sentiment.py
@@ -0,0 +1,69 @@
+'''
+Sentiment Analysis on Collected Tweets in MongoDB
+ 
+@author: Xuebin Wei
+@gmail: weixuebin@gmail.com
+website: www.lbsocial.net
+'''
+import pymongo
+from pymongo import MongoClient
+import re
+from textblob import TextBlob
+import pyodbc  # using the pyodbc library 
+
+
+'''
+coonect access
+'''
+db_file = r'C:\Project\JMU\2018 Fall\IA340\week12\tweet_sentiment.accdb' #define the location of your Access file
+
+odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s' %(db_file) # define the odbc connection parameter
+
+conn = pyodbc.connect(odbc_conn_str) # establish a database connection
+
+cursor = conn.cursor() # create a cursor
+
+
+'''
+connect mongodb database
+'''
+
+
+client = MongoClient()
+
+db = client.demo #change to your tweet database
+
+tweet_collection = db.demo # change to your tweet collection
+tweet_collection.create_index([("id", pymongo.ASCENDING)],unique = True) # add index 
+
+tweet_cursor = tweet_collection.find()
+
+for tweet in tweet_cursor:
+    try:
+        if tweet['text']:
+            tweet_text =tweet['text']
+            tweet_id = tweet['id']
+    
+            
+            text_without_url = re.sub(r"http\S+", "", tweet_text)
+            
+            result = TextBlob(text_without_url)
+            polarity = result.sentiment.polarity
+            subjectivity = result.sentiment.subjectivity
+    
+            
+            sql_insert_statement = "insert into tweet_sentiment(tweet_id,polarity,subjectivity) values({},{},{})".format(tweet_id,polarity,subjectivity) # edit the SQL statement that want to execute
+    
+            cursor.execute(sql_insert_statement) # execute the SQL statement
+    
+            cursor.commit() # let the database commit the changes in the tables
+            print("inserting {}".format(tweet_id))
+
+    except:
+
+        pass
+    
+
+print('Done!')
+cursor.close() # close the cursor
+conn.close() # close the connection to the database
